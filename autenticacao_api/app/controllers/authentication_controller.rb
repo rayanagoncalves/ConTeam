70e4f532340bcbafd493eb828e14357e26a2aadb
rayanagoncalves <rayana@MacBook-Air-de-Rayana.local>
2019-11-21 01:50:19 -0300

    Autenticaçao funcionando

--- autenticacao_api/app/controllers/authentication_controller.rb
+++ autenticacao_api/app/controllers/authentication_controller.rb
@@ -1,5 +1,5 @@
 class AuthenticationController < ApplicationController
-    before_action :authorize_request, except: :login
+    before_action :authorize_request, except: [:login, :check]
 
     # POST /auth/login
     def login
@@ -7,10 +7,29 @@ class AuthenticationController < ApplicationController
         if @user&.authenticate(params[:password])
             token = JsonWebToken.encode(user_id: @user.id)
             time = Time.now + 24.hours.to_i
+            session[:token] = token
             render json: { token: token, exp: time.strftime("%m-%d-%Y %H:%M"),
                             email: @user.email }, status: :ok
         else
             render json: { error: 'unauthorized' }, status: :unauthorized
+        end
+    end
+
+    def check
+        token = params[:token]
+        if token != ''
+            token = JsonWebToken.decode(token)
+            p token
+            @user = User.find(token[:user_id])
+            p @user
+        #    p params[:token]
+            if @user
+                render json: { status: 'ok' }, status: :ok
+            else 
+                render json: { error: 'unauthorized' }, status: :unauthorized 
+            end
+        else 
+            render json: { error: 'unauthorized' }, status: :unauthorized  
         end
     end
 
--- autenticacao_api/config/routes.rb
+++ autenticacao_api/config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   resources :users
   post '/auth/login', to: 'authentication#login'
   get 'auth/login', to: 'authentication#login'
+  get '/auth/login/check', to: 'authentication#check'
   get '/*a', to: 'application#not_found'
 end
