b1ff341c8fab6bbe1be8e001c30c84227346fe7a
rayanagoncalves <rayana@MacBook-Air-de-Rayana.local>
2019-11-21 19:44:39 -0300

    Alterando o banco para postgres

--- backend_app/config/application.rb
+++ backend_app/config/application.rb
@@ -16,5 +16,4 @@ module BackendApp
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
     end
-  end
 end
--- frontend_app/db/schema.rb
+++ frontend_app/db/schema.rb
@@ -10,17 +10,9 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_11_13_013729) do
+ActiveRecord::Schema.define(version: 0) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
-
-  create_table "containers", force: :cascade do |t|
-    t.string "name"
-    t.string "container_name"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.string "image"
-  end
 
 end
